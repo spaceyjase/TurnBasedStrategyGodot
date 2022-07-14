17388ffcfb87fe53fdb032f830522467e246e531
Jason <429978+spaceyjase@users.noreply.github.com>
2022-07-14 16:55:33 +0100

    Initial checkin.

---
+++ Unit/Model/BaseUnit.cs
@@ -0,0 +1,41 @@
+using Godot;
+
+namespace TurnBasedStrategyCourse_godot.Unit.Model
+{
+  public class BaseUnit : Spatial
+  {
+    [Export] private float movementSpeed = 4f;
+    [Export] private float stoppingDistance = .1f;
+
+    private Vector3 targetPosition = Vector3.Zero;
+
+    public override void _Process(float delta)
+    {
+      base._Process(delta);
+
+      Move(delta);
+    }
+
+    private void Move(float delta)
+    {
+      if (Translation.DistanceTo(targetPosition) < stoppingDistance) return;
+      
+      var moveDirection = (targetPosition - Translation).Normalized();
+      Translation += moveDirection * (movementSpeed * delta);
+    }
+
+    private void SetMovementDirection(Vector3 direction)
+    {
+      targetPosition = direction;
+    }
+
+    private void _on_Ground_input_event(Node camera, InputEvent @event, Vector3 position, Vector3 normal, int shape_idx)
+    {
+      if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed &&
+          eventMouseButton.ButtonIndex == 1)
+      {
+        SetMovementDirection(position);
+      }
+    }
+  }
+}
