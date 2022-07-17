ed493413b450c705632e6b8ff2dc2922c15da437
Jason <429978+spaceyjase@users.noreply.github.com>
2022-07-17 17:15:13 +0100

    Fix bone use/root animation.

--- Unit/Unit.cs
+++ Unit/Unit.cs
@@ -8,6 +8,14 @@ namespace TurnBasedStrategyCourse_godot.Unit
     [Export] private float stoppingDistance = .1f;
 
     private Vector3 targetPosition = Vector3.Zero;
+    private AnimationTree animationTree;
+    private AnimationNodeStateMachinePlayback animationStateMachine;
+    
+    public override void _Ready()
+    {
+      animationTree = GetNode<AnimationTree>("AnimationTree");
+      animationStateMachine = animationTree.Get("parameters/playback") as AnimationNodeStateMachinePlayback;
+    }
 
     public override void _Process(float delta)
     {
@@ -18,10 +26,17 @@ namespace TurnBasedStrategyCourse_godot.Unit
 
     private void Move(float delta)
     {
-      if (Translation.DistanceTo(targetPosition) < stoppingDistance) return;
-      
-      var moveDirection = (targetPosition - Translation).Normalized();
-      Translation += moveDirection * (movementSpeed * delta);
+      if (Translation.DistanceTo(targetPosition) > stoppingDistance)
+      {
+        var moveDirection = (targetPosition - Translation).Normalized();
+        Translation += moveDirection * (movementSpeed * delta);
+        
+        animationStateMachine.Travel(UnitAnimations.Running);
+      }
+      else
+      {
+        animationStateMachine.Travel(UnitAnimations.Idle);
+      }
     }
 
     private void SetMovementDirection(Vector3 direction)
@@ -29,6 +44,7 @@ namespace TurnBasedStrategyCourse_godot.Unit
       targetPosition = direction;
     }
 
+    // ReSharper disable once UnusedMember.Local
     private void _on_Ground_input_event(Node camera, InputEvent @event, Vector3 position, Vector3 normal, int shape_idx)
     {
       if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.Pressed &&
---
+++ Unit/UnitAnimations.cs
@@ -0,0 +1,8 @@
+namespace TurnBasedStrategyCourse_godot.Unit
+{
+  public abstract class UnitAnimations
+  {
+    public const string Idle = "rifle aiming idle";
+    public const string Running = "rifle run";
+  }
+}
