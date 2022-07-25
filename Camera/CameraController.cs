4a704b572a10d92409968e920f7899803a673ca2
Jason <429978+spaceyjase@users.noreply.github.com>
2022-07-25 12:59:28 +0100

    ...with rotation.

--- Camera/CameraController.cs
+++ Camera/CameraController.cs
@@ -5,19 +5,30 @@ namespace TurnBasedStrategyCourse_godot.Camera
   public class CameraController : Spatial
   {
     [Export] private float movementSpeed = 10f;
-    
+    [Export] private float rotationSpeed = 1f;
+
     public override void _Process(float delta)
     {
       base._Process(delta);
-      
-      	var leftRight = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
-      	var forwardBack = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
 
-        var movement = new Vector3(leftRight, 0f, forwardBack).Normalized();
+      var leftRight = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
+      var forwardBack = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
 
-        var globalTransform = GlobalTransform;
-        globalTransform.origin += movement * movementSpeed * delta;
-        GlobalTransform = globalTransform;
+      var movement = new Vector3(leftRight, 0f, forwardBack).Normalized();
+
+      var globalTransform = GlobalTransform;
+      globalTransform.origin += movement * movementSpeed * delta;
+      GlobalTransform = globalTransform;
+
+      if (Input.IsActionPressed("rotate_left"))
+      {
+        RotateY(-rotationSpeed * delta);
+      }
+
+      if (Input.IsActionPressed("rotate_right"))
+      {
+        RotateY(rotationSpeed * delta);
+      }
     }
   }
 }
