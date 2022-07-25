60375b14fa53c4e2641de0eba76fbcd5957cba44
Jason <429978+spaceyjase@users.noreply.github.com>
2022-07-25 14:32:10 +0100

    Camera zoom

--- Camera/CameraController.cs
+++ Camera/CameraController.cs
@@ -6,10 +6,33 @@ namespace TurnBasedStrategyCourse_godot.Camera
   {
     [Export] private float movementSpeed = 10f;
     [Export] private float rotationSpeed = 1f;
+    [Export] private float minZoom = 0.1f;
+    [Export] private float maxZoom = 2f;
+    [Export] private float zoomSpeed = 0.1f;
+
+    private float zoom = 0.5f;
+
+    public override void _Input(InputEvent @event)
+    {
+      base._Input(@event);
+
+      if (@event.IsActionPressed("zoom_in"))
+      {
+        zoom -= zoomSpeed;
+      }
+
+      if (@event.IsActionPressed("zoom_out"))
+      {
+        zoom += zoomSpeed;
+      }
+    }
 
     public override void _Process(float delta)
     {
       base._Process(delta);
+
+      zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+      Scale = new Vector3(1, 1, 1) * zoom;
 
       var leftRight = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
       var forwardBack = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
@@ -22,12 +45,12 @@ namespace TurnBasedStrategyCourse_godot.Camera
 
       if (Input.IsActionPressed("rotate_left"))
       {
-        RotateY(-rotationSpeed * delta);
+        RotateY(rotationSpeed * delta);
       }
 
       if (Input.IsActionPressed("rotate_right"))
       {
-        RotateY(rotationSpeed * delta);
+        RotateY(-rotationSpeed * delta);
       }
     }
   }
