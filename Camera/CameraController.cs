372618622623d3c86f4621a3738e3b25218e07b6
Jason A <429978+spaceyjase@users.noreply.github.com>
2022-08-01 10:53:26 +0100

    Move camera along direction it is facing.

--- Camera/CameraController.cs
+++ Camera/CameraController.cs
@@ -28,10 +28,9 @@ namespace TurnBasedStrategyCourse_godot.Camera
       var leftRight = Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left");
       var forwardBack = Input.GetActionStrength("move_down") - Input.GetActionStrength("move_up");
 
-      var movement = new Vector3(leftRight, 0f, forwardBack).Normalized();
-
       var globalTransform = GlobalTransform;
-      globalTransform.origin += movement * movementSpeed * delta;
+      globalTransform.origin += (globalTransform.basis.z * forwardBack * (movementSpeed * delta));
+      globalTransform.origin += (globalTransform.basis.x * leftRight * (movementSpeed * delta));
       GlobalTransform = globalTransform;
 
       if (Input.IsActionPressed("rotate_left"))
